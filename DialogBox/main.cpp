40543a7d9a4a4abfa6f70b8e03328d427604fed9
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-10-31 09:33:04 +0500

    HomeWork_1 DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -14,6 +14,30 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
     return 0;
 }
 
+// Функция для обработки фокуса на полях ввода
+void HandleFocus(HWND hEdit, const CHAR* szDefaultText, bool isSetFocus)
+{
+    CHAR szText[256] = {};
+    SendMessage(hEdit, WM_GETTEXT, 256, (LPARAM)szText);
+
+    if (isSetFocus)
+    {
+        // Очищаем поле только если оно содержит текст-приглашение
+        if (strcmp(szText, szDefaultText) == 0)
+        {
+            SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+        }
+    }
+    else
+    {
+        // Устанавливаем текст-приглашение, если поле пустое
+        if (strlen(szText) == 0)
+        {
+            SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)szDefaultText);
+        }
+    }
+}
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     static HWND hEditLogin;    // Дескриптор поля 'Login'
@@ -41,8 +65,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         case IDC_BUTTON_COPY:
         {
             HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
-            //Функция GetDlgItem() возвращает HWND дочернего элемента окна по его Resource ID
-            //HWND - Handle to Window (Дескриптор окна) - это число, при помощи которого можно обратиться к окну.
+            // Функция GetDlgItem() возвращает HWND дочернего элемента окна по его Resource ID
+            // HWND - Handle to Window (Дескриптор окна) - это число, при помощи которого можно обратиться к окну.
+
             CONST INT SIZE = 256;
             CHAR sz_buffer[SIZE] = {};
             SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
@@ -59,8 +84,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             EndDialog(hwnd, 0);
             break;
         }
+
 #ifdef LOGIN_PASS_1
-        // Обработка получения и потери фокуса для полей 'Login' и 'Password'
+        // Закомментированная логика обработки получения и потери фокуса для полей 'Login' и 'Password'
         if (HIWORD(wParam) == EN_SETFOCUS)
         {
             if ((HWND)lParam == hEditLogin)
@@ -100,45 +126,17 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         break;
 #endif // LOGIN_PASS_1
 
-        if (HIWORD(wParam) == EN_SETFOCUS)
+        // Для обработки фокуса на полях 'Login' и 'Password'
+        if (HIWORD(wParam) == EN_SETFOCUS || HIWORD(wParam) == EN_KILLFOCUS)
         {
             HWND hEdit = (HWND)lParam;
-            const CHAR* szDefaultText = nullptr;
+            const CHAR* szDefaultText = (hEdit == hEditLogin) ? szDefaultTextLogin :
+                (hEdit == hEditPassword) ? szDefaultTextPassword : nullptr;
 
-            // Определяем, какое поле получило фокус, и устанавливаем соответствующий текст-приглашение
-            if (hEdit == hEditLogin) szDefaultText = szDefaultTextLogin;
-            if (hEdit == hEditPassword) szDefaultText = szDefaultTextPassword;
             if (szDefaultText)
             {
-                CHAR szText[256] = {};
-                SendMessage(hEdit, WM_GETTEXT, 256, (LPARAM)szText);
-
-                // Очищаем поле только если оно содержит текст-приглашение
-                if (strcmp(szText, szDefaultText) == 0)
-                {
-                    SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
-                }
-            }
-        }
-
-        else if (HIWORD(wParam) == EN_KILLFOCUS)
-        {
-            HWND hEdit = (HWND)lParam;
-            const CHAR* szDefaultText = nullptr;
-
-            // Определяем, какое поле теряет фокус и устанавливаем соответствующий текст-приглашение
-            if (hEdit == hEditLogin) szDefaultText = szDefaultTextLogin;
-            if (hEdit == hEditPassword) szDefaultText = szDefaultTextPassword;
-            if (szDefaultText)
-            {
-                CHAR szText[256] = {};
-                SendMessage(hEdit, WM_GETTEXT, 256, (LPARAM)szText);
-
-                // Устанавливаем текст-приглашение, если поле пустое
-                if (strlen(szText) == 0)
-                {
-                    SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)szDefaultText);
-                }
+                // Вызов функции для обработки фокуса
+                HandleFocus(hEdit, szDefaultText, HIWORD(wParam) == EN_SETFOCUS);
             }
         }
         break;
