d5b393514d54310b38eda5ac4b5d72ab9ba756fe
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-11-01 21:59:49 +0500

    Class Work DONE

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -1,151 +1,66 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
-//#define LOGIN_PASS_1
+CONST CHAR g_sz_LOGIN_INVITATION[] = "Введите имя пользователя";
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-CONST CHAR* szDefaultTextLogin = "Введите имя пользователя";
-CONST CHAR* szDefaultTextPassword = "Введите пароль";
-
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
-    return 0;
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+	return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    static HWND hEditLogin;    // Дескриптор поля 'Login'
-    static HWND hEditPassword; // Дескриптор поля 'Password'
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-    switch (uMsg)
-    {
-    case WM_INITDIALOG:
-    {
-        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-        SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_EDIT_LOGIN:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_LOGIN_INVITATION) == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
 
-        // Получаем дескрипторы для полей 'Login' и 'Password' и устанавливаем текст-приглашение
-        hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-        SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)szDefaultTextLogin);
-
-        hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
-        SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)szDefaultTextPassword);
-    }
-    break;
-
-    case WM_COMMAND:
-        switch (LOWORD(wParam))
-        {
-        case IDC_BUTTON_COPY:
-        {
-            HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
-            //Функция GetDlgItem() возвращает HWND дочернего элемента окна по его Resource ID
-            //HWND - Handle to Window (Дескриптор окна) - это число, при помощи которого можно обратиться к окну.
-            CONST INT SIZE = 256;
-            CHAR sz_buffer[SIZE] = {};
-            SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-            SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-        }
-        break;
-
-        case IDOK:
-            MessageBox(hwnd, "Была нажата кнопка ОК!", "Info", MB_OK | MB_ICONINFORMATION);
-            EndDialog(hwnd, 0);
-            break;
-
-        case IDCANCEL:
-            EndDialog(hwnd, 0);
-            break;
-        }
-#ifdef LOGIN_PASS_1
-        // Обработка получения и потери фокуса для полей 'Login' и 'Password'
-        if (HIWORD(wParam) == EN_SETFOCUS)
-        {
-            if ((HWND)lParam == hEditLogin)
-            {
-                // Очищаем поле 'Login' при фокусировке
-                SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
-            }
-            if ((HWND)lParam == hEditPassword)
-            {
-                // Очищаем поле 'Password' при фокусировке
-                SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)"");
-            }
-        }
-        if (HIWORD(wParam) == EN_KILLFOCUS)
-        {
-            if ((HWND)lParam == hEditLogin)
-            {
-                // Если поле 'Login' пустое при потере фокуса, устанавливаем текст-приглашение
-                CHAR szText[256] = {};
-                SendMessage(hEditLogin, WM_GETTEXT, 256, (LPARAM)szText);
-                if (strlen(szText) == 0)
-                {
-                    SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)szDefaultTextLogin);
-                }
-            }
-            if ((HWND)lParam == hEditPassword)
-            {
-                // Если поле 'Password' пустое при потере фокуса, устанавливаем текст-приглашение
-                CHAR szText[256] = {};
-                SendMessage(hEditPassword, WM_GETTEXT, 256, (LPARAM)szText);
-                if (strlen(szText) == 0)
-                {
-                    SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)szDefaultTextPassword);
-                }
-            }
-        }
-        break;
-#endif // LOGIN_PASS_1
-
-        if (HIWORD(wParam) == EN_SETFOCUS)
-        {
-            HWND hEdit = (HWND)lParam;
-            const CHAR* szDefaultText = nullptr;
-
-            // Определяем, какое поле получило фокус, и устанавливаем соответствующий текст-приглашение
-            if (hEdit == hEditLogin) szDefaultText = szDefaultTextLogin;
-            if (hEdit == hEditPassword) szDefaultText = szDefaultTextPassword;
-            if (szDefaultText)
-            {
-                CHAR szText[256] = {};
-                SendMessage(hEdit, WM_GETTEXT, 256, (LPARAM)szText);
-
-                // Очищаем поле только если окно содержит текст-приглашение
-                if (strcmp(szText, szDefaultText) == 0)
-                {
-                    SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
-                }
-            }
-        }
-
-        if (HIWORD(wParam) == EN_KILLFOCUS)
-        {
-            HWND hEdit = (HWND)lParam;
-            const CHAR* szDefaultText = nullptr;
-
-            // Определяем, какое поле теряет фокус и устанавливаем соответствующий текст-приглашение
-            if (hEdit == hEditLogin) szDefaultText = szDefaultTextLogin;
-            if (hEdit == hEditPassword) szDefaultText = szDefaultTextPassword;
-            if (szDefaultText)
-            {
-                CHAR szText[256] = {};
-                SendMessage(hEdit, WM_GETTEXT, 256, (LPARAM)szText);
-
-                // Устанавливаем текст-приглашение, если поле пустое
-                if (strlen(szText) == 0)
-                {
-                    SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)szDefaultText);
-                }
-            }
-        }
-        break;
-
-    case WM_CLOSE:
-        EndDialog(hwnd, 0);
-        break;
-    }
-    return FALSE;
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
+				SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+		}
+		break;
+		case IDC_BUTTON_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//Фкнкция GetDlgItem() возвращает HWND дочернего элемента окна по его ResourceID.
+			//HWND - Handler to Window (Дескриптор окна) - это число, при помощи которого можно обратиться к окну.
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_BUTTON_COPY), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK!", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:	EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
 }
