12932696789bcd3662fa07c9fe7e3a29aae47be9
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-11-07 18:32:41 +0500

    AddItem to ListBox_2 DONE

--- ListBox_2/main.cpp
+++ ListBox_2/main.cpp
@@ -6,57 +6,105 @@
 CONST CHAR* g_VALUES[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
-    return 0;
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    switch (uMsg)
-    {
-    case WM_INITDIALOG:
-    {
-        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-        SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		//GetModuleHandle(NULL); возвращает hInstance нашей программы.
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-        HWND hList = GetDlgItem(hwnd, IDC_LIST1);
-        for (int i = 0; i < sizeof(g_VALUES) / sizeof(g_VALUES[0]); i++)
-        {
-            SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
-        }
-        SendMessage(hList, LB_SETCURSEL, 0, 0);
-    }
-    break;
-    case WM_COMMAND:
-        switch (LOWORD(wParam))
-        {
-        case IDOK:
-        {
-            CONST INT SIZE = 256;
-            CHAR sz_buffer[SIZE]{};
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(g_VALUES) / sizeof(g_VALUES[0]); i++)
+		{
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)g_VALUES[i]);
+		}
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_ADD:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAddItem, 0);
+			break;
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
 
-            HWND hList = GetDlgItem(hwnd, IDC_LIST1);
-            INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
-            SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
 
-            CHAR sz_message[SIZE]{};
-            sprintf(sz_message, "Вы выбрали пункт №%i со значением \"%s\".", i, sz_buffer);
-            MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
-        }
-        break;
+			CHAR sz_message[SIZE]{};
+			sprintf(sz_message, "Вы выбрали пункт №%i со значением \"%s\".", i, sz_buffer);
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+		}
+		break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
+}
+BOOL CALLBACK DlgProcAddItem(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEditAdd = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			SendMessage(hEditAdd, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 
-        case IDCANCEL:
-            EndDialog(hwnd, 0);
-            break;
-        }
-        break;
-
-    case WM_CLOSE:
-        EndDialog(hwnd, 0);
-        break;
-    }
-    return FALSE;
+			HWND hList = GetDlgItem(GetParent(hwnd), IDC_LIST1);
+			//GetParent(hwnd); возвращает родительское окно (HWND родителького окна) для указанного окна.
+			if (SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+			{
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+			}
+			else
+			{
+				INT result = MessageBox
+				(
+					hwnd,
+					"Такое вхождение уже есть в списке, хотите ввести другое значение?",
+					"Info",
+					MB_YESNO | MB_ICONQUESTION
+				);
+				if (result == IDYES)break;
+			}
+		}
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
 }
