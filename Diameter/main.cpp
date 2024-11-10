7a8078a646a261096847b2b531b808ecc33536ea
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-11-10 10:05:24 +0500

    Diameter DONE

--- Diameter/main.cpp
+++ Diameter/main.cpp
@@ -6,9 +6,10 @@
 
 using namespace std;
 
-void calculateWallThickness()
+void calculatePipeWeightByDiameters()
 {
     double outerDiameter = 0.0, innerDiameter = 0.0;
+    const double density = 7850; // Плотность стали по умолчанию в кг/м³
 
     cout << "Введите внешний диаметр трубы (мм): ";
     cin >> outerDiameter;
@@ -16,8 +17,40 @@ void calculateWallThickness()
     cout << "Введите внутренний диаметр трубы (мм): ";
     cin >> innerDiameter;
 
-    double wallThickness = (outerDiameter - innerDiameter) / 2;
-    cout << "Толщина стенки трубы: " << wallThickness << " мм" << endl;
+    double outerRadius = outerDiameter / 2 / 1000; // перевод в метры
+    double innerRadius = innerDiameter / 2 / 1000; // перевод в метры
+    double length = 1; // длина трубы в метрах (погонный метр)
+
+    double outerVolume = M_PI * pow(outerRadius, 2) * length;
+    double innerVolume = M_PI * pow(innerRadius, 2) * length;
+    double wallVolume = outerVolume - innerVolume;
+
+    double weight = wallVolume * density;
+    cout << "Вес погонного метра трубы: " << weight << " кг" << endl;
+}
+
+void calculatePipeWeightByInnerDiameterAndThickness()
+{
+    double innerDiameter = 0.0, wallThickness = 0.0;
+    const double density = 7850; // Плотность стали по умолчанию в кг/м³
+
+    cout << "Введите внутренний диаметр трубы (мм): ";
+    cin >> innerDiameter;
+
+    cout << "Введите толщину стенки трубы (мм): ";
+    cin >> wallThickness;
+
+    double outerDiameter = innerDiameter + 2 * wallThickness;
+    double outerRadius = outerDiameter / 2 / 1000; // перевод в метры
+    double innerRadius = innerDiameter / 2 / 1000; // перевод в метры
+    double length = 1; // длина трубы в метрах (погонный метр)
+
+    double outerVolume = M_PI * pow(outerRadius, 2) * length;
+    double innerVolume = M_PI * pow(innerRadius, 2) * length;
+    double wallVolume = outerVolume - innerVolume;
+
+    double weight = wallVolume * density;
+    cout << "Вес погонного метра трубы: " << weight << " кг" << endl;
 }
 
 void calculatePipeWeightWithThickness()
@@ -44,6 +77,20 @@ void calculatePipeWeightWithThickness()
     cout << "Вес погонного метра трубы: " << weight << " кг" << endl;
 }
 
+void calculateWallThickness()
+{
+    double outerDiameter = 0.0, innerDiameter = 0.0;
+
+    cout << "Введите внешний диаметр трубы (мм): ";
+    cin >> outerDiameter;
+
+    cout << "Введите внутренний диаметр трубы (мм): ";
+    cin >> innerDiameter;
+
+    double wallThickness = (outerDiameter - innerDiameter) / 2;
+    cout << "Толщина стенки трубы: " << wallThickness << " мм" << endl;
+}
+
 void calculateOuterDiameter()
 {
     double innerDiameter = 0.0, wallThickness = 0.0;
@@ -67,9 +114,11 @@ int main()
         int choice = 0;
 
         cout << "\nВыберите расчет:" << endl;
-        cout << "1 - Найти толщину стенки (по внешнему и внутреннему диаметру)" << endl;
+        cout << "1 - Найти вес погонного метра трубы (по внешнему и внутреннему диаметру)" << endl;
         cout << "2 - Найти вес погонного метра трубы (по внешнему диаметру и толщине стенки)" << endl;
-        cout << "3 - Найти внешний диаметр (по внутреннему диаметру и толщине стенки)" << endl;
+        cout << "3 - Найти вес погонного метра трубы (по внутреннему диаметру и толщине стенки)" << endl;
+        cout << "4 - Найти толщину стенки (по внешнему и внутреннему диаметру)" << endl;
+        cout << "5 - Найти внешний диаметр (по внутреннему диаметру и толщине стенки)" << endl;
         cout << "0 - Выход из программы" << endl;
         cout << "Введите номер выбора: ";
         cin >> choice;
@@ -80,15 +129,21 @@ int main()
             break; // Выход из цикла и завершение программы
         }
 
-        switch (choice) 
+        switch (choice)
         {
         case 1:
-            calculateWallThickness();
+            calculatePipeWeightByDiameters();
             break;
         case 2:
             calculatePipeWeightWithThickness();
             break;
         case 3:
+            calculatePipeWeightByInnerDiameterAndThickness();
+            break;
+        case 4:
+            calculateWallThickness();
+            break;
+        case 5:
             calculateOuterDiameter();
             break;
         default:
