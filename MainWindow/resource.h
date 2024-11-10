d90eee14f7bd14c88279c28d97d654f10e5b344e
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-11-10 23:22:52 +0500

    Cursor in Progres

--- MainWindow/resource.h
+++ MainWindow/resource.h
@@ -6,12 +6,14 @@
 #define IDI_ICON1                       102
 #define IDI_ICON2                       103
 #define IDC_CURSOR2                     107
+#define IDC_CURSOR5                     107
+#define IDR_IDC_CURSOR_011              108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
