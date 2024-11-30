a11f16b2335b8c68b3a53e9c017e808c154a4b5e
Devya_yi23@top-academy.ru <Devya_yi23@top-academy.ru>
2024-11-30 09:30:56 +0500

    DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -417,6 +417,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             if (!input && !input_operation)
             {
                 SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_CLR), 0);
+                //ZeroMemory(sz_display, SIZE);
                 sz_display[0] = 0;
             }
             if (!input && input_operation) sz_display[0] = 0;
@@ -463,7 +464,8 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             operation = 0;
             input = TRUE;
             input_operation = FALSE;
-            point_entered = FALSE;
+            //strcpy(sz_display, "0");
+            //sz_display[0] = 0;
             ZeroMemory(sz_display, SIZE);
             SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
         }
@@ -471,32 +473,35 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         // Операции (+, -, *, /)
         if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
         {
-            if (a == DBL_MIN) a = atof(sz_display);
-            if (input_operation) SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+            //SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+            if (a == DBL_MIN)a = atof(sz_display);
+            //input = FALSE;
+            if (input_operation)SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
             operation = LOWORD(wParam);
             input = FALSE;
             input_operation = TRUE;
-            point_entered = FALSE;
         }
 
         // Равно
         if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
         {
-            if (input || (b == DBL_MIN && !input)) b = atof(sz_display);
+            //if(b==DBL_MIN && !input)
+            if (input || b == DBL_MIN && !input)b = atof(sz_display);
             input = FALSE;
             switch (operation)
             {
-            case IDC_BUTTON_PLUS:   a += b; break;
-            case IDC_BUTTON_MINUS:  a -= b; break;
-            case IDC_BUTTON_ASTER:  a *= b; break;
-            case IDC_BUTTON_SLASH:  a /= b; break;
+            case IDC_BUTTON_PLUS:	a += b;	break;
+            case IDC_BUTTON_MINUS:	a -= b;	break;
+            case IDC_BUTTON_ASTER:	a *= b;	break;
+            case IDC_BUTTON_SLASH:	a /= b;	break;
             }
+            //input = FALSE;
             input_operation = FALSE;
-            point_entered = FALSE;
-            if (a == DBL_MIN) strcpy(sz_display, "0");
+            if (a == DBL_MIN)strcpy(sz_display, "0");
             else sprintf(sz_display, "%g", a);
             SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
         }
+        SetFocus(hwnd);
     }
 
     case WM_KEYDOWN:
